c8910b5851a8288a981dd7643d51447b9a4ab793
Natalio Ruiz <natalio.g.ruiz@gmail.com>
2014-08-14 23:42:07 -0300

    Module 3.14 completed!

---
+++ app/js/controllers/FilterSampleController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+eventsApp.controller('FilterSampleController', 
+	//function FilterSampleController($scope, $filter) {
+	function FilterSampleController($scope, durationsFilter) {
+		$scope.data = {};
+
+		//var durations = $filter('durations');
+		$scope.data.duration1 = durationsFilter(1);
+		$scope.data.duration2 = durationsFilter(2);
+		$scope.data.duration3 = durationsFilter(3);
+		$scope.data.duration4 = durationsFilter(4);
+
+
+	}
+);
---
+++ app/js/services/ExceptionHandler.js
@@ -0,0 +1,5 @@
+eventsApp.factory('$exceptionHandler', function() {
+	return function (exception) {
+		console.log("exception handled: " + exception.message);
+	};
+})
